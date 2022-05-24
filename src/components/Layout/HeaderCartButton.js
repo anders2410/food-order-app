ebaf23cba059f85ce588e21697bbfe78b640a28c
Anders Holt Knudsen <andhk11@gmail.com>
2022-05-24 23:18:51 +0200

    Some more changes

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,19 +1,40 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartIcon from "../Cart/CartIcon";
 
 import css from "./HeaderCartButton.module.css";
 import CartContext from "../../store/cart-context";
+import cartContext from "../../store/cart-context";
 
 const HeaderCartButton = ({ onClick }) => {
+  const [buttonIsHighlighted, setButtonIsHighlighted] = useState(false);
   const cart = useContext(CartContext);
+  const { items } = cart;
 
-  const numberOfCardItems = cart.items.reduce(
+  const numberOfCardItems = items.reduce(
     (currentValue, item) => currentValue + item.amount,
     0
   );
 
+  const btnClasses = `${css.button} ${buttonIsHighlighted ? css.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+
+    setButtonIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setButtonIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={css.button} onClick={onClick}>
+    <button className={btnClasses} onClick={onClick}>
       <span className={css.icon}>
         <CartIcon />
       </span>
--- src/store/CardProvider.js
+++ src/store/CardProvider.js
@@ -1,4 +1,5 @@
-import React, { useReducer } from "react";
+import { useReducer } from "react";
+
 import CartContext from "./cart-context";
 
 const defaultCartState = {
@@ -7,25 +8,57 @@ const defaultCartState = {
 };
 
 const cartReducer = (state, action) => {
-  console.log(action);
-  switch (action.type) {
-    case "ADD":
-      const updatedItems = state.items.concat(action.item);
-      const updatedTotalAmount = (state.totalAmount =
-        action.item.price * action.item.amount);
-      return {
-        items: updatedItems,
-        totalAmount: updatedTotalAmount,
+  if (action.type === "ADD") {
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+    const existingCartItem = state.items[existingCartItemIndex];
+    let updatedItems;
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
       };
-    case "REMOVE":
-      break;
-    default:
-      return defaultCartState;
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item);
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
   }
+  if (action.type === "REMOVE") {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+    const existingItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+    let updatedItems;
+    if (existingItem.amount === 1) {
+      updatedItems = state.items.filter((item) => item.id !== action.id);
+    } else {
+      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+
   return defaultCartState;
 };
 
-const CartProvider = ({ children }) => {
+const CartProvider = (props) => {
   const [cartState, dispatchCartAction] = useReducer(
     cartReducer,
     defaultCartState
@@ -45,8 +78,11 @@ const CartProvider = ({ children }) => {
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   };
+
   return (
-    <CartContext.Provider value={cartContext}>{children}</CartContext.Provider>
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
   );
 };
 
